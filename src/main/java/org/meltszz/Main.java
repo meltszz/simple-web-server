3e39abdb237bc0aa06503e6c3f66f9d70048ac35
Leandro Jesus <88406287+meltszz@users.noreply.github.com>
2021-11-30 11:39:27 -0100

    Instantiated and started server on Main

--- src/main/java/org/meltszz/Main.java
+++ src/main/java/org/meltszz/Main.java
@@ -3,7 +3,8 @@ package org.meltszz;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Olá mundo!");
+        Server server = new Server();
+        server.start();
     }
 
 }
