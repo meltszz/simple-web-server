6ea301d6c84bfe7071a94c29c8dd2c34238efae9
Leandro Jesus <88406287+meltszz@users.noreply.github.com>
2021-12-01 15:25:27 -0100

    ResponseHeaders class is now a Singleton class

--- src/main/java/org/meltszz/ResponseHeaders.java
+++ src/main/java/org/meltszz/ResponseHeaders.java
@@ -4,6 +4,8 @@ public class ResponseHeaders {
     private static ResponseHeaders instance;
 
     public final String NOT_FOUND = "HTTP/1.1 404 Not Found";
+    public final String OK = "HTTP/1.1 200 OK";
+    public final String NOT_IMPLEMENTED = "HTTP/1.1 501 Not Implemented";
 
     private ResponseHeaders() {
     }
@@ -16,8 +18,20 @@ public class ResponseHeaders {
         return instance;
     }
 
-    public void hello() {
+    public String documentHeader(DocumentType documentType) {
+        String header = "";
+        if (documentType == DocumentType.HTML) {
+            header = "";
+        }
 
+        if (documentType == DocumentType.CSS) {
+            header = "";
+        }
+
+        if (documentType == DocumentType.JS) {
+            header = "";
+        }
+
+        return header;
     }
-
 }
