7fd3e7b4231b5863b842f87324157be82d224198
Leandro Jesus <88406287+meltszz@users.noreply.github.com>
2021-12-01 16:33:52 -0100

    Fixed Router class Pages and IMG path

--- src/main/java/org/meltszz/Router.java
+++ src/main/java/org/meltszz/Router.java
@@ -3,12 +3,14 @@ package org.meltszz;
 import java.util.HashMap;
 
 public class Router {
-    private final String PAGES_PATH = "public/pages";
+    private final String PAGES_PATH = "src/main/resources/public/pages";
+    private final String IMG_PATH = "src/main/resources/public/public/img";
     private HashMap<String, String> routes;
 
     public Router() {
         routes = new HashMap<>();
         routes.put("/", PAGES_PATH + "/" + "home");
+        routes.put("/favicon", IMG_PATH + "/" + "favicon.ico");
     }
 
     public String getRouteURL(String route) {
