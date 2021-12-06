379c5ddb8f4b11b69e14dcec85045b4c2958e384
Leandro Jesus <88406287+meltszz@users.noreply.github.com>
2021-12-06 19:17:37 -0100

    Added ServerConfig and ServerPaths class

---
+++ src/main/java/org/meltszz/util/ServerConfig.java
@@ -0,0 +1,6 @@
+package org.meltszz.util;
+
+public class ServerConfig {
+    public static final int SERVER_PORT = 8080;
+    public static final int SERVER_MAX_THREADS = 100;
+}
---
+++ src/main/java/org/meltszz/util/ServerPaths.java
@@ -0,0 +1,9 @@
+package org.meltszz.util;
+
+public class ServerPaths {
+    public final static String PUBLIC_PATH = "src/main/resources/public/";
+    public final static String HTML_PATH = "src/main/resources/public/public/html";
+    public final static String CSS_PATH = "src/main/resources/public/public/css";
+    public final static String JS_PATH = "src/main/resources/public/public/js";
+    public final static String IMG_PATH = "src/main/resources/public/public/img";
+}
