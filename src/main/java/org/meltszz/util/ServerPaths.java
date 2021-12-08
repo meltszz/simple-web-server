1fa162bb377b451b1564ff8607d6c93e2f053792
Leandro Jesus <88406287+meltszz@users.noreply.github.com>
2021-12-08 10:56:12 -0100

    Fix to ServerPaths paths

--- src/main/java/org/meltszz/util/ServerPaths.java
+++ src/main/java/org/meltszz/util/ServerPaths.java
@@ -2,8 +2,8 @@ package org.meltszz.util;
 
 public class ServerPaths {
     public final static String PUBLIC_PATH = "src/main/resources/public/";
-    public final static String HTML_PATH = "src/main/resources/public/public/html";
-    public final static String CSS_PATH = "src/main/resources/public/public/css";
-    public final static String JS_PATH = "src/main/resources/public/public/js";
-    public final static String IMG_PATH = "src/main/resources/public/public/img";
+    public final static String HTML_PATH = "src/main/resources/public/html";
+    public final static String CSS_PATH = "src/main/resources/public/css";
+    public final static String JS_PATH = "src/main/resources/public/js";
+    public final static String IMG_PATH = "src/main/resources/public/img";
 }
