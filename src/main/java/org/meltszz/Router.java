511440f520275093860f9bb37d59c197e78f9b5c
Leandro Jesus <88406287+meltszz@users.noreply.github.com>
2021-12-11 13:11:38 -0100

    Added comments to Router class and reordred methods

--- src/main/java/org/meltszz/Router.java
+++ src/main/java/org/meltszz/Router.java
@@ -10,18 +10,20 @@ public class Router {
         routes = new HashMap<>();
     }
 
-    public String getRouteURL(String route) {
-        return hasRoute(route) ? routes.get(route) : "";
-    }
-
-    public boolean hasRoute(String route) {
-        return routes.containsKey(route);
-    }
-
+    /***
+     * Add Route
+     * Adds a route to the route map
+     * @param url Request URL i.e: /index, /login, etc.
+     * @param path Root path of the file that will be served.
+     */
     public void addRoute(String url, String path) {
         routes.put(url, path);
     }
 
+    /***
+     * Create Page Routes
+     * Creates an url for every html page.
+     */
     public void createPageRoutes() {
        File htmlDirectory = new File(ServerPaths.HTML_PATH);
        String[] htmlFilesNames = htmlDirectory.list();
@@ -31,6 +33,10 @@ public class Router {
        }
     }
 
+    /***
+     * Create Public Routes
+     * Creates an url for every public resource.
+     */
     public void createPublicRoutes() {
        for (String directoryName: ServerPaths.ALL_PUBLIC_PATHS) {
            File directory = new File(directoryName);
@@ -41,7 +47,26 @@ public class Router {
        }
     }
 
+    /***
+     * Create Home Page Route
+     * Links a html file to the / route.
+     * @param htmlFileName html file name
+     */
     public void createHomePageRoute(String htmlFileName) {
         this.addRoute("/", ServerPaths.HTML_PATH + "/" + htmlFileName + ".html");
+    }
+
+    public String getRouteURL(String route) {
+        return hasRoute(route) ? routes.get(route) : "";
+    }
+
+    /***
+     * Has Route
+     * Checks if routes map has a specific route.
+     * @param route url route i.e: /index
+     * @return true if has route false if doesn't have the route
+     */
+    public boolean hasRoute(String route) {
+        return routes.containsKey(route);
     }
 }
